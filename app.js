459592d5472cd3b46dc71e130ada075cf2a721f7
FranklinPTorres <105072367+FranklinPTorres@users.noreply.github.com>
2023-05-30 08:58:43 +0100

    new updates in the todolist

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use('view engine', 'ejs');
+let item = []
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
    let today = new Date();
